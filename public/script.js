6dc58e73afefd4efd5957568900e16dcabf3a006
Veronika Demko <demkoveronika06@gmail.com>
2024-09-20 15:50:41 +0300

    Add functionality and responsive design

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
-console.log('Hello world')
+document.addEventListener('DOMContentLoaded', function() {
+  const reviews = document.querySelectorAll('.reviews__bottom-review--item');
+  const reviewsContainer = document.querySelector('.reviews__bottom-review--list');
+  const reviewWidth = 370;
+  let currentIndex = 0;
+
+  function showReview(index) {
+    const offset = index * -reviewWidth;
+    reviewsContainer.style.transform = `translateX(${offset}px)`; 
+  }
+
+  document.getElementById('next').addEventListener('click', function() {
+    if (currentIndex < reviews.length - 1) {
+      currentIndex++; 
+      showReview(currentIndex);
+    }
+  });
+
+  document.getElementById('prev').addEventListener('click', function() {
+    if (currentIndex > 0) {
+      currentIndex--; 
+      showReview(currentIndex);
+    }
+  });
+
+  showReview(currentIndex);
+});
